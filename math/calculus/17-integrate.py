3fce4ce2ef283ee5bfb28d2cb118b7c50be84a4b
Ezio-33 <8691@holbertonstudents.com>
2024-12-12 20:43:58 +0100

    fin du projet

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -1,1 +1,35 @@
 #!/usr/bin/env python3
+"""
+Module qui calcule l'intégrale d'un polynôme
+"""
+
+def poly_integral(poly, C=0):
+    """
+    Args:
+        poly: Liste des coefficients du polynôme
+        C: Constante d'intégration (par défaut 0)
+
+    Return:
+        Liste représentant l'intégrale du polynôme
+        None: Si poly n'est pas valide ou si C n'est pas un entier
+    """
+    if not isinstance(poly, list) or not isinstance(C, int):
+        return None
+    
+    if len(poly) == 0:
+        return None
+
+    # L'intégrale d'une constante est Cx + C
+    if len(poly) == 1:
+        return [C, poly[0]]
+
+    # Calcul de l'intégrale
+    integral = [C]  # Commence avec la constante d'intégration
+    for i in range(len(poly)):
+        coef = poly[i] / (i + 1)
+        # Si le coefficient est un entier, le convertir
+        if coef.is_integer():
+            coef = int(coef)
+        integral.append(coef)
+
+    return integral
