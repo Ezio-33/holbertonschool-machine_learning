c84287caea9ed62a8ce038d5874cbc091677dbdd
Ezio-33 <8691@holbertonstudents.com>
2024-12-12 10:28:37 +0100

    task 6 à 12

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -1,1 +1,38 @@
 #!/usr/bin/env python3
+"""
+Module qui calcule la dérivée d'un polynôme
+"""
+
+
+def poly_derivative(poly):
+    """
+    Calcule la dérivée d'un polynôme
+
+    Args:
+        Liste des coefficients du polynôme
+            index 0 = terme constant
+            index 1 = coefficient de x
+            index 2 = coefficient de x²
+            etc.
+
+    Return:
+        Liste des coefficients de la dérivée
+        None: Si poly n'est pas valide
+    """
+    if not isinstance(poly, list) or len(poly) == 0:
+        return None
+
+    # Si le polynôme est constant
+    if len(poly) == 1:
+        return [0]
+
+    # Calcul de la dérivée
+    derivative = []
+    for i in range(1, len(poly)):
+        derivative.append(poly[i] * i)
+
+    # Si tous les coefficients sont 0
+    if all(coef == 0 for coef in derivative):
+        return [0]
+
+    return derivative
--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+"""    Calcule la somme des carrés des nombres de 1 à n
+"""
+
+
+def summation_i_squared(n):
+    """
+    Args:
+        n: nombre entier positif
+    Return:
+        somme des carrés ou None si n invalide
+    """
+    if not isinstance(n, int) or n < 1:
+        return None
+    # Formule : sum(i²) = (n * (n + 1) * (2n + 1)) / 6
+    return (n * (n + 1) * (2 * n + 1)) // 6
