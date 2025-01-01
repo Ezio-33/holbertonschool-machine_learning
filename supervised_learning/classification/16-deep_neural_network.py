0f6885dc110f206967cc2814f6b6e671ce3d860a
Ezio-33 <8691@holbertonstudents.com>
2025-01-01 20:15:53 +0100

    task 17

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -1,36 +1,48 @@
 #!/usr/bin/env python3
 """
-new class: DeepNeuralNetwork, task  16:  DeepNeuralNetwork
+Module définissant un réseau de neurones profond
+pour la classification binaire
 """
 import numpy as np
 
 
 class DeepNeuralNetwork:
-    """ define a new class"""
+    """
+    Classe définissant un réseau de neurones profond
+    pour la classification binaire
+    """
 
     def __init__(self, nx, layers):
-        if type(nx) is not int:
-            raise TypeError("nx must be an integer")
-        elif nx < 1:
-            raise ValueError("nx must be a positive integer")
-        self.nx = nx
+        """
+        Initialise un réseau de neurones profond
 
-        if type(layers) is not list or len(layers) == 0:
+        Args:
+            nx (int): nombre de caractéristiques d'entrée
+            layers (list): liste contenant le nombre de neurones par couche
+        """
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if not isinstance(layers, list) or len(layers) == 0:
+            raise TypeError("layers must be a list of positive integers")
+        if not all(isinstance(n, int) and n > 0 for n in layers):
             raise TypeError("layers must be a list of positive integers")
 
-        self.layers = layers
-        self.L = len(layers)
-        self.weights = dict()
-        self.cache = dict()
+        # Attributs publics
+        self.L = len(layers)  # Nombre de couches
+        self.cache = {}       # Stocke les valeurs intermédiaires
+        self.weights = {}     # Stocke les poids et biais
 
-        for i in range(len(layers)):
-            if type(layers[i]) is not int or layers[i] < 0:
-                raise TypeError("layers must be a list of positive integers")
+        # Initialisation des poids avec la méthode He et al.
+        for index_couche in range(self.L):
+            couche_size = layers[index_couche]
+            input_size = nx if index_couche == 0 else layers[index_couche - 1]
 
-            if i == 0:
-                self.weights['W1'] = np.random.randn(
-                    layers[i], nx) * np.sqrt(2 / nx)
-            else:
-                self.weights['W{}'.format(i + 1)] = np.random.randn(
-                    layers[i], layers[i - 1]) * np.sqrt(2 / layers[i - 1])
-            self.weights['b{}'.format(i + 1)] = np.zeros((layers[i], 1))
+            # Initialisation des poids
+            self.weights[f'W{index_couche+1}'] = np.random.randn(
+                couche_size, input_size
+            ) * np.sqrt(2 / input_size)
+
+            # Initialisation des biais
+            self.weights[f'b{index_couche+1}'] = np.zeros((couche_size, 1))
