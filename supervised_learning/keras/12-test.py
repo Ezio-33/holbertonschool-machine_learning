837e6d5ea639f9cd51f068347eefb4be898c183e
Ezio-33 <8691@holbertonstudents.com>
2025-01-16 18:16:25 +0100

    correction task 10

---
+++ supervised_learning/keras/12-test.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+Module pour tester un modèle de deep learning
+"""
+import tensorflow.keras as K
+
+
+def test_model(network, data, labels):
+    """
+    Teste un modèle de deep learning
+
+    Arguments:
+        network: modèle à tester
+        data: numpy.ndarray contenant les données de test
+        labels: numpy.ndarray one-hot contenant les étiquettes de test
+
+    Returns:
+        Les performances du modèle sur les données de test
+    """
+    return network.evaluate(
+        x=data,
+        y=labels,
+        verbose=0
+    )
