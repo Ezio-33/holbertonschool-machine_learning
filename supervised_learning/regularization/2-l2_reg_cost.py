eec8f15034803d80292ca1872a0fa0295fa385f7
Ezio-33 <8691@holbertonstudents.com>
2025-01-29 13:16:57 +0100

    task 2

--- supervised_learning/regularization/2-l2_reg_cost.py
+++ supervised_learning/regularization/2-l2_reg_cost.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """
-Calcule le coût d'un réseau de neurones avec régularisation L2
+Calcule le coût d'un réseau de neurones avec régularisation L2,
+en retournant un tenseur contenant le coût pour chaque couche.
 """
 
 import tensorflow as tf
@@ -8,22 +9,28 @@ import tensorflow as tf
 
 def l2_reg_cost(cost, model):
     """
-    Calcule le coût total incluant la régularisation L2
+    Calcule le coût total, couche par couche, incluant la régularisation L2
 
     Arguments:
         cost: tensor contenant le coût du réseau sans régularisation L2
-        model: modèle Keras qui inclut des couches avec régularisation L2
+        model: un modèle Keras qui inclut des couches avec régularisation L2
 
     Returns:
-        tensor contenant le coût total avec régularisation L2
+        tensor de forme (nb_couches,) contenant le coût total
+        pour chacune des couches, incluant la régularisation L2
     """
-    # Additionne le coût initial et toutes les pertes de régularisation du modèle
-    l2_cost = cost
-    
-    # Parcours toutes les couches du modèle
+    # Nous construisons un vecteur, chaque entrée correspondant au coût + pertes
+    # de régularisation de la couche correspondante
+    costs = []
     for layer in model.layers:
-        # Ajoute les pertes de régularisation de chaque couche si elles existent  
+        # Si la couche ne possède pas de pénalités de régularisation,
+        # elle n'ajoute rien au coût
         if len(layer.losses) > 0:
-            l2_cost += tf.math.add_n(layer.losses)
-    
-    return l2_cost
+            # On ajoute au coût initial la somme des régularisations de la couche
+            costs.append(cost + tf.math.add_n(layer.losses))
+        else:
+            # Sinon, c'est juste le coût de base
+            costs.append(cost)
+
+    # Empile chaque coût en un seul tenseur 1D
+    return tf.stack(costs)
