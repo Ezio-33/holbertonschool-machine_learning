b3ff14f0fb941978bcaf46df260aa085a1661557
Ezio-33 <8691@holbertonstudents.com>
2025-01-29 18:41:04 +0100

    task 2

--- supervised_learning/regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/regularization/1-l2_reg_gradient_descent.py
@@ -1,22 +1,20 @@
 #!/usr/bin/env python3
 """
-Coût de régularisation L2
+L2 Regularization Cost.
 """
-
 import tensorflow as tf
 
 
-def l2_reg_cost(cost):
+def l2_reg_cost(cost, model):
     """
-    Fonction qui calcule le coût d'un réseau de neurones avec
-    régularisation L2
-    Arguments:
-     - cost est un tenseur contenant le coût du réseau sans
-        régularisation L2
-    Renvoie:
-    Un tenseur contenant le coût du réseau en tenant compte de
-    la régularisation L2
-    """
-    L2_cost = cost + tf.losses.get_regularization_losses()
+    Calcule le coût d'un réseau de neurones avec régularisation L2.
 
-    return L2_cost
+    Args:
+        cost (float): Le coût initial du réseau de neurones
+        sans régularisation.
+        model (object): Le modèle contenant les pertes de régularisation L2.
+
+    Returns:
+        float: Le coût du réseau de neurones incluant la régularisation L2.
+    """
+    return cost + model.losses
