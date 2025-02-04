b1483ae03e5ac35a27a5214d87f11cbae8b21946
Ezio-33 <8691@holbertonstudents.com>
2025-02-04 15:01:44 +0100

    correction task 1

--- math/convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -6,37 +6,41 @@ import numpy as np
 
 def convolve_grayscale_same(images, kernel):
     """
-    Effectue une convolution same sur un batch d'images
+    Effectue une convolution same sur des images en niveaux de gris
 
     Args:
-        images : ndarray shape (m, h, w)
-        kernel : ndarray shape (kh, kw)
+        images : ndarray shape (m, h, w) contenant les images
+        kernel : ndarray shape (kh, kw) contenant le kernel
 
     Returns:
-        ndarray shape (m, h, w) des images convoluées
+        ndarray contenant les images convoluées
     """
     m, h, w = images.shape
     kh, kw = kernel.shape
 
-    # Calcul du padding
-    ph = (kh - 1) // 2
-    pw = (kw - 1) // 2
+    # Gestion du padding pour kernels pairs et impairs
+    ph = (kh - int(kh % 2 == 1)) // 2
+    pw = (kw - int(kw % 2 == 1)) // 2
+
+    # Calcul des dimensions de sortie
+    h_out = h
+    w_out = w
 
     # Application du padding
-    padded_images = np.pad(images,
-                           ((0, 0), (ph, ph), (pw, pw)),
-                           mode='constant')
+    padded = np.pad(images, 
+                    ((0, 0), (ph, ph), (pw, pw)),
+                    mode='constant',
+                    constant_values=0)
 
-    # Initialisation sortie même dimension qu'entrée
-    output = np.zeros((m, h, w))
+    # Initialisation de la sortie
+    output = np.zeros((m, h_out, w_out))
 
-    # Convolution valide sur l'image padée
-    for i in range(h):
-        for j in range(w):
-            # Extraction de la fenêtre correspondante
-            window = padded_images[:, i:i + kh, j:j + kw]
-            # Application du kernel et somme
-            result = np.sum(window * kernel[np.newaxis, :, :], axis=(1, 2))
-            output[:, i, j] = result
+    # Convolution
+    for i in range(h_out):
+        for j in range(w_out):
+            # Extraction de la fenêtre correcte
+            window = padded[:, i:i+kh, j:j+kw]
+            # Multiplication et somme sur les axes corrects
+            output[:, i, j] = np.sum(window * kernel, axis=(1, 2))
 
     return output
