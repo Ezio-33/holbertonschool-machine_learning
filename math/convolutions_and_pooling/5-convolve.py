a66ef94262edd702722258dc01edbb8ccb4d4f93
Ezio-33 <8691@holbertonstudents.com>
2025-02-05 12:35:07 +0100

    Correction task 5

--- math/convolutions_and_pooling/5-convolve.py
+++ math/convolutions_and_pooling/5-convolve.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Module de convolution avec multiples kernels"""
+"""Convolution multi-kernels avec gestion avancée du padding et stride"""
 
 import numpy as np
 
@@ -19,51 +19,45 @@ def convolve(images, kernels, padding='same', stride=(1, 1)):
     """
     m, h, w, c = images.shape
     kh, kw, kc, nc = kernels.shape
-
-    # Vérification compatibilité canaux
-    if kc != c:
-        raise ValueError("Kernel channels must match image channels")
-
     sh, sw = stride
 
-    # Gestion du padding
+    # Vérification des canaux
+    if kc != c:
+        raise ValueError("Les canaux du kernel doivent correspondre à l'image")
+
+    # Calcul du padding avec gestion du stride
     if padding == 'same':
-        ph = (kh - 1) // 2
-        pw = (kw - 1) // 2
+        ph = ((h - 1) * sh + kh - h) // 2 + 1
+        pw = ((w - 1) * sw + kw - w) // 2 + 1
     elif padding == 'valid':
         ph, pw = 0, 0
     else:
         ph, pw = padding
 
-    # Application padding (ne pas padder les canaux)
+    # Application du padding
     padded = np.pad(images,
                     ((0, 0), (ph, ph), (pw, pw), (0, 0)),
                     mode='constant')
 
-    # Calcul dimensions de sortie
+    # Calcul des dimensions de sortie
     h_out = (h + 2 * ph - kh) // sh + 1
     w_out = (w + 2 * pw - kw) // sw + 1
 
-    # Initialisation sortie avec nc canaux
     output = np.zeros((m, h_out, w_out, nc))
 
-    # Boucle sur chaque kernel (3ème boucle autorisée)
-    for k in range(nc):
-        kernel = kernels[:, :, :, k]  # Kernel courant
+    # Parcours spatial optimisé
+    for i in range(h_out):
+        for j in range(w_out):
+            h_start = i * sh
+            h_end = h_start + kh
+            w_start = j * sw
+            w_end = w_start + kw
 
-        # Boucles spatiales
-        for i in range(h_out):
-            for j in range(w_out):
-                # Calcul des positions avec stride
-                h_start = i * sh
-                h_end = h_start + kh
-                w_start = j * sw
-                w_end = w_start + kw
+            # Extraction fenêtre vectorisée
+            window = padded[:, h_start:h_end, w_start:w_end, :]
 
-                # Extraction fenêtre (m, kh, kw, c)
-                window = padded[:, h_start:h_end, w_start:w_end, :]
-
-                # Produit + somme sur kh, kw, c
-                output[:, i, j, k] = np.sum(window * kernel, axis=(1, 2, 3))
+            # Calcul simultané pour tous les kernels
+            output[:, i, j, :] = np.tensordot(
+                window, kernels, axes=([1, 2, 3], [0, 1, 2]))
 
     return output
