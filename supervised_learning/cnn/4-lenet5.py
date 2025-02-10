8f1f8516f3fd19f1a832b617b4b26f4d0978e2dc
Ezio-33 <8691@holbertonstudents.com>
2025-02-10 16:20:26 +0100

    correction task 4

--- supervised_learning/cnn/4-lenet5.py
+++ supervised_learning/cnn/4-lenet5.py
@@ -1,83 +1,53 @@
 #!/usr/bin/env python3
-"""Module implémentant LeNet-5 avec TensorFlow 1.x"""
-
-import tensorflow.compat.v1 as tf
+"""LeNet-5 avec TensorFlow 1.x"""
+import tensorflow as tf  # Import de TensorFlow 1.x
 
 
 def lenet5(x, y):
-    """
-    Construit LeNet-5 modifié pour MNIST
+    # 1. Initialisation "He Normal" (compatible TF1)
+    k_init = tf.contrib.layers.variance_scaling_initializer()
 
-    Args:
-        x: Images d'entrée (m, 28, 28, 1)
-        y: Labels one-hot (m, 10)
+    # 2. Couche Conv1 : 6 filtres 5x5, padding 'same' pour conserver la taille
+    conv1 = tf.layers.Conv2D(filters=6, kernel_size=5, padding='same',
+                             activation=tf.nn.relu,
+                             kernel_initializer=k_init)(x)
 
-    Returns:
-        y_pred: Prédictions (softmax)
-        train_op: Opération d'entraînement Adam
-        loss: Fonction de perte
-        acc: Précision
-    """
-    # Initialisation He pour les poids
-    init = tf.contrib.layers.variance_scaling_initializer()
+    # 3. Pooling1 : Réduction 2x2 avec pas de 2
+    pool1 = tf.layers.MaxPooling2D(pool_size=[2, 2], strides=2)(conv1)
 
-    # Première couche convolutive (6 filtres 5x5)
-    conv1 = tf.layers.Conv2D(
-        filters=6,
-        kernel_size=5,
-        padding='same',
-        activation=tf.nn.relu,
-        kernel_initializer=init)(x)
+    # 4. Couche Conv2 : 16 filtres 5x5, padding 'valid' (pas de padding)
+    conv2 = tf.layers.Conv2D(filters=16, kernel_size=5, padding='valid',
+                             activation=tf.nn.relu,
+                             kernel_initializer=k_init)(pool1)
 
-    # Premier pooling (2x2)
-    pool1 = tf.layers.MaxPooling2D(
-        pool_size=[2, 2],
-        strides=2)(conv1)
+    # 5. Pooling2 : Même paramètres
+    pool2 = tf.layers.MaxPooling2D(pool_size=[2, 2], strides=2)(conv2)
 
-    # Deuxième couche convolutive (16 filtres 5x5)
-    conv2 = tf.layers.Conv2D(
-        filters=16,
-        kernel_size=5,
-        padding='valid',
-        activation=tf.nn.relu,
-        kernel_initializer=init)(pool1)
-
-    # Deuxième pooling (2x2)
-    pool2 = tf.layers.MaxPooling2D(
-        pool_size=[2, 2],
-        strides=2)(conv2)
-
-    # Aplatissement pour les couches denses
+    # 6. Mise à plat pour les couches denses
     flat = tf.layers.Flatten()(pool2)
 
-    # Première couche dense (120 neurones)
-    dense1 = tf.layers.Dense(
-        units=120,
-        activation=tf.nn.relu,
-        kernel_initializer=init)(flat)
+    # 7. Couche Dense1 : 120 neurones avec ReLU
+    fc1 = tf.layers.Dense(units=120, activation=tf.nn.relu,
+                          kernel_initializer=k_init)(flat)
 
-    # Deuxième couche dense (84 neurones)
-    dense2 = tf.layers.Dense(
-        units=84,
-        activation=tf.nn.relu,
-        kernel_initializer=init)(dense1)
+    # 8. Couche Dense2 : 84 neurones avec ReLU
+    fc2 = tf.layers.Dense(units=84, activation=tf.nn.relu,
+                          kernel_initializer=k_init)(fc1)
 
-    # Couche de sortie (10 classes)
-    logits = tf.layers.Dense(
-        units=10,
-        kernel_initializer=init)(dense2)
+    # 9. Couche de Sortie : 10 neurones (sans activation)
+    logits = tf.layers.Dense(units=10, kernel_initializer=k_init)(fc2)
 
-    # Activation softmax séparée
+    # 10. Application du Softmax pour obtenir les probabilités
     y_pred = tf.nn.softmax(logits)
 
-    # Calcul de la perte (cross-entropy)
-    loss = tf.losses.softmax_cross_entropy(y, logits)
+    # 11. Calcul de la perte (cross-entropy) sur les logits directement
+    loss = tf.reduce_mean(tf.losses.softmax_cross_entropy(y, logits))
 
-    # Optimisation avec Adam
+    # 12. Optimiseur Adam
     train_op = tf.train.AdamOptimizer().minimize(loss)
 
-    # Calcul de la précision
-    correct = tf.equal(tf.argmax(y, 1), tf.argmax(logits, 1))
-    acc = tf.reduce_mean(tf.cast(correct, tf.float32))
+    # 13. Calcul de la précision
+    correct_pred = tf.equal(tf.argmax(y, 1), tf.argmax(logits, 1))
+    acc = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
 
     return y_pred, train_op, loss, acc
