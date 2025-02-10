3d1fb32ba89861ff4a2d5bcf7f424dc4eee46cc8
Ezio-33 <8691@holbertonstudents.com>
2025-02-10 17:47:46 +0100

    correctoin task 4

--- supervised_learning/cnn/4-lenet5.py
+++ supervised_learning/cnn/4-lenet5.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Module implémentant l'architecture LeNet-5 modifiée"""
+"""Module implémentant l'architecture LeNet-5 avec TensorFlow 1"""
 
 import tensorflow.compat.v1 as tf
 tf.disable_eager_execution()
@@ -7,75 +7,81 @@ tf.disable_eager_execution()
 
 def lenet5(x, y):
     """
-    Construit le réseau LeNet-5 pour reconnaître les chiffres
+    Construit l'architecture LeNet-5 modifiée
 
     Args:
-        x: Images d'entrée (m, 28, 28, 1)
-        y: Labels attendus (m, 10)
+        x: Placeholder pour les images d'entrée (m, 28, 28, 1)
+        y: Placeholder pour les labels one-hot (m, 10)
+
+    Returns:
+        y_pred: Tenseur de sortie avec softmax
+        train_op: Opération d'entraînement Adam
+        loss: Tenseur de perte
+        accuracy: Tenseur de précision
     """
-    # Initialisation spéciale pour les poids
+    # Initialisation He pour tous les poids
     init = tf.contrib.layers.variance_scaling_initializer()
 
-    # Première couche: 6 filtres de 5x5
+    # Première couche convolutive
     conv1 = tf.layers.Conv2D(
-        filters=6,          # Nombre de loupes différentes
-        kernel_size=5,      # Taille de chaque loupe
-        padding='same',     # On garde les bords
-        activation=tf.nn.relu,  # On garde que les valeurs positives
+        filters=6,
+        kernel_size=5,
+        padding='same',
+        activation=tf.nn.relu,
         kernel_initializer=init
     )(x)
 
-    # Réduction de taille par 2
+    # Premier max pooling
     pool1 = tf.layers.MaxPooling2D(
-        pool_size=2,
+        pool_size=[2, 2],
         strides=2
     )(conv1)
 
-    # Deuxième couche: 16 filtres de 5x5
+    # Deuxième couche convolutive
     conv2 = tf.layers.Conv2D(
         filters=16,
         kernel_size=5,
-        padding='valid',    # On ne garde pas les bords
+        padding='valid',
         activation=tf.nn.relu,
         kernel_initializer=init
     )(pool1)
 
-    # Nouvelle réduction de taille
+    # Deuxième max pooling
     pool2 = tf.layers.MaxPooling2D(
-        pool_size=2,
+        pool_size=[2, 2],
         strides=2
     )(conv2)
 
-    # On met tout à plat
+    # Aplatissement
     flatten = tf.layers.Flatten()(pool2)
 
-    # Première couche dense: 120 neurones
+    # Première couche dense
     dense1 = tf.layers.Dense(
         units=120,
         activation=tf.nn.relu,
         kernel_initializer=init
     )(flatten)
 
-    # Deuxième couche dense: 84 neurones
+    # Deuxième couche dense
     dense2 = tf.layers.Dense(
         units=84,
         activation=tf.nn.relu,
         kernel_initializer=init
     )(dense1)
 
-    # Couche de sortie: 10 neurones (un par chiffre)
+    # Couche de sortie
     logits = tf.layers.Dense(
         units=10,
         kernel_initializer=init
     )(dense2)
 
-    # Probabilités pour chaque chiffre
+    # Prédictions avec softmax
     y_pred = tf.nn.softmax(logits)
 
-    # Calcul de l'erreur
+    # Calcul de la perte
     loss = tf.losses.softmax_cross_entropy(y, logits)
 
-    # Optimisation pour réduire l'erreur
+    # Optimisation
     train_op = tf.train.AdamOptimizer().minimize(loss)
 
     # Calcul de la précision
