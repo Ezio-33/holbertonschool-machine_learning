fb6fd5f445653c67ad8c0371b6c7cda443b8588b
Ezio-33 <8691@holbertonstudents.com>
2025-02-10 15:14:14 +0100

    correction task 0

--- supervised_learning/cnn/0-conv_forward.py
+++ supervised_learning/cnn/0-conv_forward.py
@@ -1,71 +1,64 @@
 #!/usr/bin/env python3
-"""Implémentation de la propagation avant pour une couche convolutive"""
+"""Propagation avant pour une couche convolutive"""
 
 import numpy as np
 
 
 def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
     """
-    Effectue la propagation avant pour une couche convolutive
+    Effectue la propagation avant d'une couche convolutive.
 
     Args:
-        A_prev: Sortie de la couche précédente (m, h_prev, w_prev, c_prev)
-        W: Filtres de convolution (kh, kw, c_prev, c_new)
+        A_prev: Entrée de forme (m, h_prev, w_prev, c_prev)
+        W: Filtres (kh, kw, c_prev, c_new)
         b: Biais (1, 1, 1, c_new)
         activation: Fonction d'activation
         padding: 'same' ou 'valid'
-        stride: Tuple (sh, sw)
+        stride: Pas (sh, sw)
 
     Returns:
-        Sortie de la couche convolutive après activation
+        Sortie activée de la convolution
     """
 
-    # Extraction des dimensions
+    # Dimensions d'entrée
     m, h_prev, w_prev, c_prev = A_prev.shape
     kh, kw, _, c_new = W.shape
     sh, sw = stride
 
-    # Calcul des dimensions de sortie
+    # Calcul du padding selon le type
     if padding == "same":
-        h_out = np.ceil(h_prev / sh).astype(int)
-        w_out = np.ceil(w_prev / sw).astype(int)
+        ph = ((h_prev - 1) * sh + kh - h_prev) // 2
+        pw = ((w_prev - 1) * sw + kw - w_prev) // 2
+    else:  # 'valid'
+        ph, pw = 0, 0
 
-        # Calcul du padding nécessaire
-        pad_h = max((h_out - 1) * sh + kh - h_prev, 0)
-        pad_top = pad_h // 2
-        pad_bottom = pad_h - pad_top
+    # Application du padding
+    A_pad = np.pad(A_prev,
+                   ((0, 0), (ph, ph), (pw, pw), (0, 0)),
+                   mode='constant')
 
-        pad_w = max((w_out - 1) * sw + kw - w_prev, 0)
-        pad_left = pad_w // 2
-        pad_right = pad_w - pad_left
-
-        # Application du padding
-        A_pad = np.pad(A_prev,
-                       ((0, 0), (pad_top, pad_bottom),
-                        (pad_left, pad_right), (0, 0)),
-                       mode='constant')
-    else:
-        h_out = (h_prev - kh) // sh + 1
-        w_out = (w_prev - kw) // sw + 1
-        A_pad = A_prev
+    # Dimensions de sortie
+    h_out = (h_prev + 2 * ph - kh) // sh + 1
+    w_out = (w_prev + 2 * pw - kw) // sw + 1
 
     # Initialisation de la sortie
     Z = np.zeros((m, h_out, w_out, c_new))
 
-    # Calcul de la convolution fenêtre par fenêtre
+    # Parcours de chaque position de sortie
     for i in range(h_out):
         for j in range(w_out):
-            # Position de la fenêtre
-            v_start = i * sh
-            v_end = v_start + kh
-            h_start = j * sw
-            h_end = h_start + kw
+            # Calcul de la fenêtre
+            vert_start = i * sh
+            vert_end = vert_start + kh
+            horiz_start = j * sw
+            horiz_end = horiz_start + kw
 
-            # Extraction de la fenêtre
-            window = A_pad[:, v_start:v_end, h_start:h_end, :]
+            # Extraction de la région
+            a_slice = A_pad[:, vert_start:vert_end, horiz_start:horiz_end, :]
 
-            # Calcul du produit tensoriel pour tous les exemples et filtres
-            Z[:, i, j, :] = np.tensordot(window, W, axes=(
-                [1, 2, 3], [0, 1, 2])) + b[0, 0, 0, :]
+            # Calcul convolution + biais pour tous les filtres
+            for c in range(c_new):
+                Z[:, i, j, c] = np.sum(
+                    a_slice * W[..., c], axis=(1, 2, 3)) + b[..., c]
 
     return activation(Z)
