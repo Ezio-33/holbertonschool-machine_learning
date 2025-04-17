292784d117c08da2ff80a8f2145438e43c0530c7
Ezio-33 <8691@holbertonstudents.com>
2025-04-17 18:53:04 +0200

    correction pycode

--- unsupervised_learning/hyperparameter_tuning/5-bayes_opt.py
+++ unsupervised_learning/hyperparameter_tuning/5-bayes_opt.py
@@ -7,10 +7,21 @@ import numpy as np
 from scipy.stats import norm
 GP = __import__('2-gp').GaussianProcess
 
+
 class BayesianOptimization:
     """Classe d'optimisation bayésienne avec processus gaussien"""
-    
-    def __init__(self, f, X_init, Y_init, bounds, ac_samples, l=1, sigma_f=1, xsi=0.01, minimize=True):
+
+    def __init__(
+            self,
+            f,
+            X_init,
+            Y_init,
+            bounds,
+            ac_samples,
+            l=1,
+            sigma_f=1,
+            xsi=0.01,
+            minimize=True):
         """Initialisation des paramètres"""
         self.f = f
         self.gp = GP(X_init, Y_init, l, sigma_f)
@@ -33,7 +44,7 @@ class BayesianOptimization:
         Z = imp / sigma
         ei = imp * norm.cdf(Z) + sigma * norm.pdf(Z)
         ei[sigma == 0] = 0
-        
+
         X_next = self.X_s[np.argmax(ei)]
         return X_next.reshape(1,), ei
 
@@ -42,11 +53,17 @@ class BayesianOptimization:
         for _ in range(iterations):
             X_next, _ = self.acquisition()
             X_next = X_next.reshape(1, 1)  # Format (1,1) pour comparaison
-            
+
             # Vérification stricte des doublons avec tolérance numérique
-            if np.any(np.all(np.isclose(self.gp.X, X_next, atol=1e-8), axis=1)):
+            if np.any(
+                np.all(
+                    np.isclose(
+                        self.gp.X,
+                        X_next,
+                        atol=1e-8),
+                    axis=1)):
                 break
-                
+
             Y_next = self.f(X_next)
             self.gp.update(X_next, Y_next)
 
@@ -55,5 +72,5 @@ class BayesianOptimization:
             idx = np.argmin(self.gp.Y)
         else:
             idx = np.argmax(self.gp.Y)
-            
+
         return self.gp.X[idx].reshape(1,), self.gp.Y[idx].reshape(1,)
