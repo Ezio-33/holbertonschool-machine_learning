60ef688293f9cd468b05a93d2d2be5ebee1a5702
Ezio-33 <8691@holbertonstudents.com>
2025-04-28 23:02:21 +0200

    task 0

--- unsupervised_learning/gan/0-simple_gan.py
+++ unsupervised_learning/gan/0-simple_gan.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python3
 """
-Module Simple_GAN - Implémentation corrigée d'un GAN de base avec eager execution
+Module Simple_GAN - Implémentation corrigée avec gestion des métriques
 """
 import tensorflow as tf
 from tensorflow import keras
 
 class Simple_GAN(keras.Model):
-    def __init__(self, generator, discriminator, latent_generator, real_examples, batch_size=200, disc_iter=2, learning_rate=0.005):
+    def __init__(self, generator, discriminator, latent_generator, real_examples, 
+                 batch_size=200, disc_iter=2, learning_rate=0.005):
         super().__init__()
-        
-        # Activation explicite du mode eager (optionnel pour TF 2.x)
-        tf.config.run_functions_eagerly(True)
         
         # Initialisation des composants
         self.generator = generator
@@ -21,8 +19,20 @@ class Simple_GAN(keras.Model):
         self.disc_iter = disc_iter
 
         # Configuration des optimiseurs
-        self.generator_optimizer = keras.optimizers.Adam(learning_rate=learning_rate, beta_1=0.5, beta_2=0.9)
-        self.discriminator_optimizer = keras.optimizers.Adam(learning_rate=learning_rate, beta_1=0.5, beta_2=0.9)
+        self.g_optimizer = keras.optimizers.Adam(
+            learning_rate=learning_rate, 
+            beta_1=0.5, 
+            beta_2=0.9
+        )
+        self.d_optimizer = keras.optimizers.Adam(
+            learning_rate=learning_rate,
+            beta_1=0.5,
+            beta_2=0.9
+        )
+
+        # Déclaration des métriques
+        self.disc_loss_tracker = keras.metrics.Mean(name="discr_loss")
+        self.gen_loss_tracker = keras.metrics.Mean(name="gen_loss")
 
     def get_fake_sample(self, size=None, training=False):
         size = size or self.batch_size
@@ -34,28 +44,45 @@ class Simple_GAN(keras.Model):
         return tf.gather(self.real_examples, tf.random.shuffle(indices)[:size])
 
     def train_step(self, data):
+        # Réinitialisation des métriques
+        self.disc_loss_tracker.reset_state()
+        self.gen_loss_tracker.reset_state()
+
         # Entraînement du discriminateur
         for _ in range(self.disc_iter):
-            with tf.GradientTape() as disc_tape:
+            with tf.GradientTape() as d_tape:
                 real_samples = self.get_real_sample()
                 fake_samples = self.get_fake_sample()
                 
                 pred_real = self.discriminator(real_samples, training=True)
                 pred_fake = self.discriminator(fake_samples, training=True)
                 
-                disc_loss = tf.reduce_mean(tf.square(pred_real - 1.0)) + tf.reduce_mean(tf.square(pred_fake + 1.0))
+                # Calcul de la perte Wasserstein
+                d_loss = tf.reduce_mean(pred_fake) - tf.reduce_mean(pred_real)
             
-            gradients = disc_tape.gradient(disc_loss, self.discriminator.trainable_variables)
-            self.discriminator_optimizer.apply_gradients(zip(gradients, self.discriminator.trainable_variables))
+            # Application des gradients
+            d_gradients = d_tape.gradient(d_loss, self.discriminator.trainable_variables)
+            self.d_optimizer.apply_gradients(zip(d_gradients, self.discriminator.trainable_variables))
+            self.disc_loss_tracker.update_state(d_loss)
 
         # Entraînement du générateur
-        with tf.GradientTape() as gen_tape:
-            fake_samples = self.get_fake_sample()
-            pred_fake = self.discriminator(fake_samples, training=False)
+        with tf.GradientTape() as g_tape:
+            generated_samples = self.get_fake_sample()
+            pred_fake = self.discriminator(generated_samples, training=False)
             
-            gen_loss = tf.reduce_mean(tf.square(pred_fake - 1.0))
+            # Calcul de la perte du générateur
+            g_loss = -tf.reduce_mean(pred_fake)
         
-        gradients = gen_tape.gradient(gen_loss, self.generator.trainable_variables)
-        self.generator_optimizer.apply_gradients(zip(gradients, self.generator.trainable_variables))
+        # Mise à jour des poids
+        g_gradients = g_tape.gradient(g_loss, self.generator.trainable_variables)
+        self.g_optimizer.apply_gradients(zip(g_gradients, self.generator.trainable_variables))
+        self.gen_loss_tracker.update_state(g_loss)
 
-        return {"discr_loss": disc_loss, "gen_loss": gen_loss}
+        return {
+            "discr_loss": self.disc_loss_tracker.result(),
+            "gen_loss": self.gen_loss_tracker.result()
+        }
+
+    @property
+    def metrics(self):
+        return [self.disc_loss_tracker, self.gen_loss_tracker]
