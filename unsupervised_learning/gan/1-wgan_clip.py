458c434534ff053e6f4c919c075742643712144d
Ezio-33 <8691@holbertonstudents.com>
2025-04-29 15:57:10 +0200

    task 1

---
+++ unsupervised_learning/gan/1-wgan_clip.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python3
+"""
+Module WGAN_clip - Implémentation d'un WGAN avec poids clipsés
+"""
+
+import tensorflow as tf
+from tensorflow import keras
+
+
+class WGAN_clip(keras.Model):
+    """
+    Classe WGAN avec contrainte de clipping des poids
+    Args:
+        generator: Modèle générateur
+        discriminator: Modèle discriminateur (appelé 'critic' dans WGAN)
+        latent_generator: Fonction générant des vecteurs latents
+        real_examples: Jeu de données réel
+        batch_size: Taille des lots
+        disc_iter: Nombre d'itérations du discriminateur par étape
+        learning_rate: Taux d'apprentissage
+    """
+
+    def __init__(
+            self,
+            generator,
+            discriminator,
+            latent_generator,
+            real_examples,
+            batch_size=200,
+            disc_iter=2,
+            learning_rate=0.005):
+        super().__init__()
+
+        # Initialisation des composants
+        self.generator = generator
+        self.discriminator = discriminator
+        self.latent_generator = latent_generator
+        self.real_examples = real_examples
+        self.batch_size = batch_size
+        self.disc_iter = disc_iter
+
+        # Configuration des optimiseurs
+        self.generator.optimizer = keras.optimizers.Adam(
+            learning_rate=learning_rate,
+            beta_1=0.5,
+            beta_2=0.9
+        )
+        self.discriminator.optimizer = keras.optimizers.Adam(
+            learning_rate=learning_rate,
+            beta_1=0.5,
+            beta_2=0.9
+        )
+
+        # Définition des fonctions de perte WGAN
+        # Le générateur veut maximiser la sortie
+        self.generator.loss = lambda x: -tf.reduce_mean(x)
+        self.discriminator.loss = lambda real_out, fake_out: tf.reduce_mean(
+            fake_out) - tf.reduce_mean(real_out)
+
+    def get_fake_sample(self, size=None, training=False):
+        """Génère un batch de données fausses"""
+        size = size or self.batch_size
+        return self.generator(self.latent_generator(size), training=training)
+
+    def get_real_sample(self, size=None):
+        """Sélectionne un batch de données réelles"""
+        size = size or self.batch_size
+        indices = tf.range(tf.shape(self.real_examples)[0])
+        return tf.gather(self.real_examples, tf.random.shuffle(indices)[:size])
+
+    def train_step(self, data):
+        """Une étape complète d'entraînement"""
+
+        # 1. Entraînement du discriminateur (critic)
+        for _ in range(self.disc_iter):
+            with tf.GradientTape() as tape:
+                real_samples = self.get_real_sample()
+                fake_samples = self.get_fake_sample()
+
+                real_out = self.discriminator(real_samples, training=True)
+                fake_out = self.discriminator(fake_samples, training=True)
+
+                d_loss = self.discriminator.loss(real_out, fake_out)
+
+            # Calcul et application des gradients
+            gradients = tape.gradient(
+                d_loss, self.discriminator.trainable_variables)
+            self.discriminator.optimizer.apply_gradients(
+                zip(gradients, self.discriminator.trainable_variables)
+            )
+
+            # Clipping des poids entre [-1, 1]
+            for weight in self.discriminator.trainable_variables:
+                weight.assign(tf.clip_by_value(weight, -1.0, 1.0))
+
+        # 2. Entraînement du générateur
+        with tf.GradientTape() as tape:
+            generated_samples = self.get_fake_sample()
+            fake_out = self.discriminator(generated_samples, training=False)
+
+            g_loss = self.generator.loss(fake_out)
+
+        # Mise à jour des poids du générateur
+        gradients = tape.gradient(g_loss, self.generator.trainable_variables)
+        self.generator.optimizer.apply_gradients(
+            zip(gradients, self.generator.trainable_variables)
+        )
+
+        return {"d_loss": d_loss, "g_loss": g_loss}
