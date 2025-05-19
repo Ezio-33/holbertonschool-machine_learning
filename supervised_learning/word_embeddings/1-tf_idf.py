e1c5d98fe12a74bf461e9e13fbbda1a220a31868
Ezio-33 <8691@holbertonstudents.com>
2025-05-19 17:10:55 +0200

    corraction task 1

--- supervised_learning/word_embeddings/1-tf_idf.py
+++ supervised_learning/word_embeddings/1-tf_idf.py
@@ -1,51 +1,26 @@
 #!/usr/bin/env python3
 """
-Crée une matrice d'embedding avec la méthode TF-IDF.
+Crée une matrice d'embedding avec la méthode TF-IDF à l'aide de sklearn.
 """
 
 import numpy as np
-import math
-import re
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 
 def tf_idf(sentences, vocab=None):
     """
-    Génère une matrice d'embedding selon la méthode TF-IDF.
+    Génère une matrice TF-IDF à partir d'une liste de phrases.
 
     Args:
-        sentences (list): Liste des phrases (strings)
-        vocab (list, optional): Liste des mots à utiliser.
-		Si None, on extrait tous les mots.
+        sentences (list): Liste de chaînes de caractères (phrases).
+        vocab (list): Liste optionnelle de mots (features) à utiliser.
 
     Returns:
         tuple:
-            embeddings (numpy.ndarray): Matrice TF-IDF (phrases x mots)
-            features (list): Liste triée des mots utilisés
+            numpy.ndarray: matrice TF-IDF
+            list: liste des features utilisées
     """
-    cleaned = [
-        [re.sub(r"[.,!?]", "", w).lower() for w in s.split()]
-        for s in sentences
-    ]
-
-    if vocab is None:
-        features = sorted({word for sent in cleaned for word in sent})
-    else:
-        features = vocab
-
-    s = len(sentences)
-    f = len(features)
-    embeddings = np.zeros((s, f))
-
-    for i, sent in enumerate(cleaned):
-        for j, word in enumerate(features):
-            tf = sent.count(word) / len(sent)
-
-            doc_count = sum(1 for s in cleaned if word in s)
-            if doc_count == 0:
-                idf = 0
-            else:
-                idf = math.log(s / doc_count)
-
-            embeddings[i][j] = tf * idf
-
-    return embeddings, features
+    vectorizer = TfidfVectorizer(vocabulary=vocab)
+    tfidf_matrix = vectorizer.fit_transform(sentences).todense()
+    features = vectorizer.get_feature_names_out()
+    return tfidf_matrix, features
