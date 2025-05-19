33749f39625ff4b0304ce8e1a78e433d5f0fefce
Ezio-33 <8691@holbertonstudents.com>
2025-05-19 16:44:32 +0200

    Task 1

---
+++ supervised_learning/word_embeddings/1-tf_idf.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+"""
+Crée une matrice d'embedding avec la méthode TF-IDF.
+"""
+
+import numpy as np
+import math
+import re
+
+
+def tf_idf(sentences, vocab=None):
+    """
+    Génère une matrice d'embedding selon la méthode TF-IDF.
+
+    Args:
+        sentences (list): Liste des phrases (strings)
+        vocab (list, optional): Liste des mots à utiliser.
+		Si None, on extrait tous les mots.
+
+    Returns:
+        tuple:
+            embeddings (numpy.ndarray): Matrice TF-IDF (phrases x mots)
+            features (list): Liste triée des mots utilisés
+    """
+    # Nettoyage et découpe
+    cleaned = [
+        [re.sub(r"[.,!?]", "", w).lower() for w in s.split()]
+        for s in sentences
+    ]
+
+    if vocab is None:
+        features = sorted({word for sent in cleaned for word in sent})
+    else:
+        features = vocab
+
+    s = len(sentences)
+    f = len(features)
+    embeddings = np.zeros((s, f))
+
+    for i, sent in enumerate(cleaned):
+        for j, word in enumerate(features):
+            tf = sent.count(word) / len(sent)
+
+            doc_count = sum(1 for s in cleaned if word in s)
+            if doc_count == 0:
+                idf = 0
+            else:
+                idf = math.log(s / doc_count)
+
+            embeddings[i][j] = tf * idf
+
+    return embeddings, features
