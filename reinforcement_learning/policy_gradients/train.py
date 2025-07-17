da81c7354308d70759f9d7a94499d7d3c05352ab
Ezio-33 <8691@holbertonstudents.com>
2025-07-17 16:38:00 +0200

    task 2

--- reinforcement_learning/policy_gradients/train.py
+++ reinforcement_learning/policy_gradients/train.py
@@ -21,19 +21,14 @@ def train(env, nb_episodes, alpha=0.000045, gamma=0.98, show_result=False):
     # obtenir le nombre d'états et d'actions de l'environnement gymnasium
     n_states, n_actions = env.observation_space.shape[0], env.action_space.n
 
+    # Initialiser la graine pour les poids
+    np.random.seed(0)
     weights = np.random.rand(n_states, n_actions)
 
     for i in range(nb_episodes):
 
         # définir done à False pour l'état initial
         done = False
-
-        # afficher un épisode tous les 1000 épisodes
-        # si show_result est défini à True
-        if show_result and i % 1000 == 0:
-            env = gym.make('CartPole-v1', render_mode="human")
-        else:
-            env = gym.make('CartPole-v1', render_mode=None)
 
         state, _ = env.reset()
         rewards = []
@@ -48,7 +43,9 @@ def train(env, nb_episodes, alpha=0.000045, gamma=0.98, show_result=False):
             gradients.append(grad)
             rewards.append(reward)
 
-            weights += alpha * sum([g * (gamma ** t) * r for t, (g, r) in enumerate(zip(gradients, rewards))])
+            weights += alpha * sum([grad * (gamma ** t) * reward 
+                                   for t, (grad, reward) in enumerate(
+                                    zip(gradients, rewards))])
 
             # quitter après 500 étapes
             if done:
