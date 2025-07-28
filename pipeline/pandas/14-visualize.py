ad2a4afa01bc201c492eadd91c49f6067d5dc764
Ezio-33 <8691@holbertonstudents.com>
2025-07-28 14:53:41 +0200

    amelioration des commentaires

--- pipeline/pandas/14-visualize.py
+++ pipeline/pandas/14-visualize.py
@@ -1,23 +1,29 @@
 #!/usr/bin/env python3
 
+#!/usr/bin/env python3
+
+# Importation des bibliothèques
 import matplotlib.pyplot as plt
 import pandas as pd
 from_file = __import__('2-from_file').from_file
 
+# Chargement du dataset Bitcoin de Coinbase
 df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
 
-# YOUR CODE HERE
-
+# Préparation des données
 df = df.drop(columns=["Weighted_Price"])
 df['Timestamp'] = pd.to_datetime(df['Timestamp'], unit='s')
-df = df.rename(columns={'Timestamp': 'Date'})
-df = df.set_index("Date")
+df = df.rename(columns={'Timestamp': 'Date'}).set_index("Date")
+
+# Gestion des valeurs manquantes
 df["Close"] = df["Close"].ffill()
 df["High"] = df["High"].fillna(df["Close"])
 df["Low"] = df["Low"].fillna(df["Close"])
 df["Open"] = df["Open"].fillna(df["Close"])
 df[["Volume_(BTC)", "Volume_(Currency)"]] = df[[
     "Volume_(BTC)", "Volume_(Currency)"]].fillna(value=0)
+
+# Filtrage et agrégation par jour depuis 2017
 df = df.loc[df.index >= pd.Timestamp("2017-01-01")]
 df = df.groupby(df.index.floor('D')).agg({
     "High": "max",
@@ -29,7 +35,7 @@ df = df.groupby(df.index.floor('D')).agg({
 })
 print(df)
 
-# plotting
+# Création du graphique d'analyse des données Bitcoin
 ax2 = df[["High",
           "Low",
           "Open", "Close", "Volume_(BTC)",
