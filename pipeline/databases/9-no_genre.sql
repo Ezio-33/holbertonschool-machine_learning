a54995cc91283ef5a11aa81ada7e3e0899a0b640
Ezio-33 <8691@holbertonstudents.com>
2025-07-31 20:12:21 +0200

    correction task 9

--- pipeline/databases/9-no_genre.sql
+++ pipeline/databases/9-no_genre.sql
@@ -1,6 +1,7 @@
--- Liste toutes les séries avec leurs genres (même celles sans genre)
--- Format: titre - genre_id, affiche NULL si pas de genre
+-- Liste toutes les séries qui n'ont AUCUN genre lié
+-- Format: titre - genre_id (NULL), trié par titre puis genre_id
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NULL
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
