0494c1dd87f78ae5027e08aee2c9e2be10de2f1e
Ezio-33 <8691@holbertonstudents.com>
2025-07-31 20:29:05 +0200

    task 12

---
+++ pipeline/databases/12-rating_genres.sql
@@ -0,0 +1,8 @@
+-- Liste tous les genres triés par leur note totale (ordre décroissant)
+-- Somme des ratings de toutes les séries de chaque genre
+SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating
+FROM tv_genres
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_show_ratings ON tv_show_genres.show_id = tv_show_ratings.show_id
+GROUP BY tv_genres.name
+ORDER BY rating DESC;
